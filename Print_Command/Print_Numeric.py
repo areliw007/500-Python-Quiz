bc98e823007e1098735417a6a7eb9553a6905867
areliw <areliw11111@gmail.com>
2025-08-05 02:57:10 +0700

    Add Print_String+Numeric.py exercises Q21-Q30

---
+++ Print_Command/Print_Numeric.py
@@ -0,0 +1,17 @@
+#%% Q16
+print('25')
+
+#%% Q17
+print(f'{float(100):.6f}')
+
+#%% Q18
+from  math import pi
+print(f'{pi}')
+
+#%% Q19
+a=2
+print(f'{a}')
+
+#%% Q20
+a=12.5
+print(f'{a}')
---
+++ Print_Command/Print_String.py
@@ -0,0 +1,45 @@
+#%% Q1
+print('Hello')
+
+#%%Q2
+print('Hello World!')
+
+#%% Q3
+print('Hello python!')
+
+#%% Q4
+print('Merry X,Mas')
+
+#%% Q5
+print('I want to ask "Why don\'t you drive to work? "')
+
+#%% Q6
+print('"I don\'t have a car"')
+
+#%% Q7
+print('You got a new job !? That\'s so exciting')
+
+#%% Q8
+print('สวัสดีวันจันทร์')
+
+#%% Q9
+print('ความแตกต่างระหว่างคนเก่งกับคนที่ไม่เก่งคือ "การใช้เวลาว่างให้เป็นประโยชน์"')
+
+#%% Q10
+print('/\/\/\\')
+
+#%% Q11
+print(">>> a\n and\n ant")
+
+#%% Q12
+print('\t *\n *\t *\t *\n\t *')
+
+#%% Q13
+print('*\t+\t*\n+\t*\t+\n*\t+\t*')
+
+#%% Q14
+print('>>>> Just because something\nthinks differently from you,\ndoes that mean it\'s not thinking?')
+
+#%% Q15
+print("\ \t\t /\n \tx \t\n \ \t\t /")
+
